152f86660ceedd1231e2e67dee06731f75255166
Ris-CodedIt <123630177+Ris-CodedIt@users.noreply.github.com>
2024-08-14 12:05:49 +0200

    pro mangt

--- Project-Management/src/App.jsx
+++ Project-Management/src/App.jsx
@@ -2,6 +2,7 @@ import ProjectSideBar  from "./components/ProjectSideBar";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import { useState } from "react";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectState, setprojectState] = useState({selectedProjectId:undefined, projects:[]})
@@ -14,7 +15,36 @@ function App() {
       }
     })
   }
+  
+  const handleSelectProject =(id)=>{
+    setprojectState(prevState=>{
+      return {
+        ...prevState,
+        selectedProjectId:id
+      }
+    })
+  }
 
+
+  const handleCancelAddProject = ()=>{
+    setprojectState(prevState=>{
+      return {
+        ...prevState,
+        selectedProjectId:undefined
+      }
+    })
+  }
+
+
+  const handleDeleteProject = ()=>{
+    setprojectState(prevState=>{
+      return {
+        ...prevState,
+        selectedProjectId:undefined,
+        projects: prevState.projects.filter((project)=> project.id !== prevState.selectedProjectId)
+      }
+    })
+  }
 
   const handleAddProject = (projectData)=>{
     setprojectState(prevState=>{
@@ -30,12 +60,14 @@ function App() {
       }
     })
   }
+  
 
+  const selectedProject = projectState.projects.find(project=> project.id === projectState.selectedProjectId)
 
-  let content;
+  let content = <SelectedProject project={selectedProject}  onDeleteProject={handleDeleteProject}/>
 
   if(projectState.selectedProjectId ===null){
-    content = <NewProject onAdd={handleAddProject}/>
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
   }else if(projectState.selectedProjectId === undefined){
     content = <NoProjectSelected osStartAddProject={handleStartAddProject}/>
   }
@@ -46,7 +78,7 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSideBar onStartAddProject={handleStartAddProject} projects={projectState.projects}/>
+      <ProjectSideBar onStartAddProject={handleStartAddProject} projects={projectState.projects} onSelectProject={handleSelectProject} selectedProjectId={projectState.selectedProjectId}/>
       {content}
     </main>
   );
--- Project-Management/src/components/NewProject.jsx
+++ Project-Management/src/components/NewProject.jsx
@@ -1,7 +1,9 @@
 import { useRef } from "react"
 import Input from "./Input"
+import Modal from "./Modal"
 
-const NewProject = ( {onAdd}) => {
+const NewProject = ( {onAdd, onCancel}) => {
+    const modal = useRef()
    const title = useRef()
    const description = useRef()
    const dueDate = useRef()
@@ -12,6 +14,10 @@ const NewProject = ( {onAdd}) => {
        const enteredDueDate = dueDate.current.value
 
     //    validation
+       if(enteredTitle.trim()==='' || enteredDescription.trim() === '' || enteredDueDate.trim()===''){
+          modal.current.open()
+          return
+       }
        onAdd({
         title:enteredTitle,
         description:enteredDescription,
@@ -20,9 +26,15 @@ const NewProject = ( {onAdd}) => {
 
    }
   return (
+    <>
+        <Modal ref={modal} buttonCaption={"Close"}>
+            <h2 className='text-xl font-bold text-stone-700 my-4'> Invalid Input</h2>
+            <p  className='text-stone-600 mb-4'>Opps... loks like you forgot to enter a value.</p>
+            <p  className='text-stone-600 mb-4'>Please make sure you provide a valid value for every input field</p>
+        </Modal>
     <div className="w-[35rem] mt-16 ">
         <menu className="flex items-center justify-end gap-4 my-4">
-            <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+            <li><button onClick={onCancel} className="text-stone-800 hover:text-stone-950">Cancel</button></li>
             <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
         </menu>
         <div>
@@ -32,6 +44,8 @@ const NewProject = ( {onAdd}) => {
 
         </div>
     </div>
+    </>
+    
   )
 }
 
